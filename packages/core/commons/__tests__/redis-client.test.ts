987089da4cdcf16eb0320d297095e179944f933c
Ucandoit <yukan.zhao@gmail.com>
2020-05-09 13:58:54 +0200

    feature: init tests using jest

--- packages/core/commons/__tests__/redis-client.test.ts
+++ packages/core/commons/__tests__/redis-client.test.ts
@@ -1,3 +1,16 @@
-test('test', () => {
-  expect(1 + 2).toEqual(3);
+import redisClient from '../src/redis-client';
+
+beforeAll(() => {
+  redisClient.start();
+  redisClient.set('test1', '1');
+  redisClient.set('test2', '2');
+});
+
+afterAll(() => {
+  redisClient.disconnect();
+});
+
+test('redis get', async () => {
+  const val = await redisClient.get('test1');
+  expect(val).toMatchSnapshot();
 });
--- packages/core/commons/src/redis-client.ts
+++ packages/core/commons/src/redis-client.ts
@@ -1,26 +1,32 @@
 import { getLogger } from 'log4js';
-import redis from 'redis';
+import { ClientOpts, createClient, RedisClient as NodeRedisClient } from 'redis';
 
 class RedisClient {
   private logger = getLogger(RedisClient.name);
 
-  private redisClient: redis.RedisClient;
+  private redisClient: NodeRedisClient;
 
-  start = (): void => {
+  start = (options?: ClientOpts): void => {
     this.logger.info('Create redis client.');
-    this.redisClient = redis.createClient({
-      host: 'nobot-redis',
-      port: 6379,
-      // eslint-disable-next-line @typescript-eslint/camelcase
-      retry_strategy: (options) => {
-        this.logger.info('Retrying.');
-        if (options.attempt > 5) {
-          throw new Error('Retry attempts reached.');
-        }
-        return 5000;
-      }
-    });
+    this.redisClient = createClient(options);
   };
+
+  disconnect = (): void => {
+    this.redisClient.quit();
+  };
+
+  // {
+  //   host: 'nobot-redis',
+  //   port: 6379,
+  //   // eslint-disable-next-line @typescript-eslint/camelcase
+  //   retry_strategy: (options) => {
+  //     this.logger.info('Retrying.');
+  //     if (options.attempt > 5) {
+  //       throw new Error('Retry attempts reached.');
+  //     }
+  //     return 5000;
+  //   }
+  // }
 
   get = (key: string): Promise<string> => {
     return new Promise((resolve, reject) => {
